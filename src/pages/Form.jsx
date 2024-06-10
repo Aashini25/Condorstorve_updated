dd59dd96aa8e4bc7e8ef6c2ac4da278c314e5834
Subashini M <129865184+Aashini25@users.noreply.github.com>
2024-06-10 15:30:14 +0530

    Add files via upload

---
+++ src/pages/Form.jsx
@@ -0,0 +1,161 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+
+const Form = () => {
+  return (
+    <div className="w-full relative bg-gainsboro-100 h-[3018px] overflow-hidden text-left text-base text-black font-montserrat">
+      <div className="absolute top-[0px] left-[0px] [background:linear-gradient(90deg,_#a0a3ff,_#c78eed)] w-[1512px] h-[840px]" />
+      <div className="absolute top-[165px] left-[134px] font-medium font-poppins text-steelblue">
+        {" "}
+        Enquire form
+      </div>
+      <div className="absolute top-[165px] left-[72px] font-medium font-poppins text-steelblue">{`Home >`}</div>
+      <Button
+        className="w-[209px] absolute top-[2067px] left-[calc(50%_-_138px)] shadow-[0px_1px_2px_rgba(0,_0,_0,_0.05)]"
+        variant="primary"
+      >
+        Submit
+      </Button>
+      <div className="absolute top-[989px] left-[307px] text-xl leading-[32px] flex items-center w-[146px]">
+        First Name
+      </div>
+      <div className="absolute top-[1112px] left-[307px] text-xl leading-[32px] flex items-center w-[146px]">
+        Last Name
+      </div>
+      <div className="absolute top-[1226px] left-[307px] text-xl leading-[32px] flex items-center w-[146px]">
+        Email
+      </div>
+      <div className="absolute top-[1341px] left-[308px] text-xl leading-[32px] flex items-center w-[196px]">
+        Phone number
+      </div>
+      <div className="absolute top-[1455px] left-[307px] text-xl leading-[32px] flex items-center w-[196px]">
+        Address
+      </div>
+      <div className="absolute top-[1570px] left-[308px] text-xl leading-[32px] flex items-center w-[310px]">
+        Locations you interested in
+      </div>
+      <div className="absolute top-[1693px] left-[309px] text-xl leading-[32px] flex items-center w-[310px]">
+        When planning to visit
+      </div>
+      <div className="absolute top-[1816px] left-[310px] text-xl leading-[32px] flex items-center w-[310px]">
+        Best contact time
+      </div>
+      <div className="absolute top-[1956px] left-[346px] text-xl leading-[32px] flex items-center w-[310px]">
+        I agree to the privacy policy
+      </div>
+      <div className="absolute top-[1027px] left-[calc(50%_-_449px)] [filter:drop-shadow(0px_1px_2px_rgba(0,_0,_0,_0.05))] rounded-lg box-border w-[866px] h-14 text-center text-thistle border-[1px] border-solid border-mediumslateblue">
+        <div className="absolute top-[calc(50%_-_8.6px)] left-[146.2px] w-5 h-5 overflow-hidden" />
+        <div className="absolute top-[16px] left-[24px] leading-[24px] font-medium">
+          Enter your first name
+        </div>
+      </div>
+      <div className="absolute top-[1150px] left-[calc(50%_-_449px)] [filter:drop-shadow(0px_1px_2px_rgba(0,_0,_0,_0.05))] rounded-lg box-border w-[866px] h-14 text-center text-thistle border-[1px] border-solid border-mediumslateblue">
+        <div className="absolute top-[calc(50%_-_8.6px)] left-[146.2px] w-5 h-5 overflow-hidden" />
+        <div className="absolute top-[15px] left-[22px] leading-[24px] font-medium">
+          Enter your Last name
+        </div>
+      </div>
+      <div className="absolute top-[1264px] left-[calc(50%_-_449px)] [filter:drop-shadow(0px_1px_2px_rgba(0,_0,_0,_0.05))] rounded-lg box-border w-[866px] h-14 text-center text-thistle border-[1px] border-solid border-mediumslateblue">
+        <div className="absolute top-[calc(50%_-_8.6px)] left-[146.2px] w-5 h-5 overflow-hidden" />
+        <div className="absolute top-[15px] left-[26px] leading-[24px] font-medium">
+          Enter your email address
+        </div>
+      </div>
+      <div className="absolute top-[1379px] left-[calc(50%_-_448px)] [filter:drop-shadow(0px_1px_2px_rgba(0,_0,_0,_0.05))] rounded-lg box-border w-[866px] h-14 text-center text-thistle border-[1px] border-solid border-mediumslateblue">
+        <div className="absolute top-[calc(50%_-_8.6px)] left-[146.2px] w-5 h-5 overflow-hidden" />
+        <div className="absolute top-[15px] left-[21px] leading-[24px] font-medium">
+          Enter your phone number
+        </div>
+      </div>
+      <div className="absolute top-[1493px] left-[calc(50%_-_449px)] [filter:drop-shadow(0px_1px_2px_rgba(0,_0,_0,_0.05))] rounded-lg box-border w-[866px] h-14 text-center text-thistle border-[1px] border-solid border-mediumslateblue">
+        <div className="absolute top-[calc(50%_-_8.6px)] left-[146.2px] w-5 h-5 overflow-hidden" />
+        <div className="absolute top-[15px] left-[20px] leading-[24px] font-medium">
+          Enter your school name
+        </div>
+      </div>
+      <div className="absolute top-[1608px] left-[calc(50%_-_449px)] [filter:drop-shadow(0px_1px_2px_rgba(0,_0,_0,_0.05))] rounded-lg box-border w-[866px] h-14 text-center text-thistle border-[1px] border-solid border-mediumslateblue">
+        <div className="absolute top-[calc(50%_-_8.6px)] left-[146.2px] w-5 h-5 overflow-hidden" />
+        <div className="absolute top-[15px] left-[17px] leading-[24px] font-medium">
+          Enter your current grade
+        </div>
+      </div>
+      <div className="absolute top-[1731px] left-[calc(50%_-_448px)] [filter:drop-shadow(0px_1px_2px_rgba(0,_0,_0,_0.05))] rounded-lg box-border w-[866px] h-14 text-center text-thistle border-[1px] border-solid border-mediumslateblue">
+        <div className="absolute top-[calc(50%_-_8.6px)] left-[146.2px] w-5 h-5 overflow-hidden" />
+        <div className="absolute top-[15px] left-[16px] leading-[24px] font-medium">
+          Enter your city name
+        </div>
+      </div>
+      <div className="absolute top-[1731px] left-[calc(50%_-_448px)] [filter:drop-shadow(0px_1px_2px_rgba(0,_0,_0,_0.05))] rounded-lg box-border w-[866px] h-14 text-center text-thistle border-[1px] border-solid border-mediumslateblue">
+        <div className="absolute top-[calc(50%_-_8.6px)] left-[146.2px] w-5 h-5 overflow-hidden" />
+        <div className="absolute top-[15px] left-[16px] leading-[24px] font-medium">
+          Enter your city name
+        </div>
+      </div>
+      <div className="absolute top-[1854px] left-[calc(50%_-_447px)] [filter:drop-shadow(0px_1px_2px_rgba(0,_0,_0,_0.05))] rounded-lg box-border w-[866px] h-14 text-center text-thistle border-[1px] border-solid border-mediumslateblue">
+        <div className="absolute top-[calc(50%_-_8.6px)] left-[146.2px] w-5 h-5 overflow-hidden" />
+        <div className="absolute top-[15px] left-[31px] leading-[24px] font-medium">
+          Please select one
+        </div>
+      </div>
+      <div className="absolute top-[1964px] left-[308px] rounded-[50%] box-border w-[15px] h-[15px] border-[1px] border-solid border-black" />
+      <div className="absolute top-[2506px] left-[95px] w-[1321px] flex flex-row items-start justify-start py-0 pr-[78px] pl-[84px] box-border gap-[68px] text-4xl text-grey-scale-black-50 font-circular-std">
+        <div className="flex-1 flex flex-col items-start justify-start gap-[64px]">
+          <div className="flex flex-col items-start justify-start gap-[32px]">
+            <img
+              className="w-[139px] relative h-[103px] object-cover"
+              alt=""
+              src="/image-196removebgpreview-21@2x.png"
+            />
+            <div className="w-[278px] relative leading-[160%] inline-block">
+              <p className="m-0">
+                Contrary to popular belief, Lorem Ipsum is not simply random
+                text. It has roots
+              </p>
+              <p className="m-0">
+                in a piece of classical Latin literature from 45 BC.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row items-start justify-center gap-[32px]">
+            <img className="w-8 relative h-8" alt="" src="/group.svg" />
+            <img className="w-8 relative h-8" alt="" src="/group1.svg" />
+            <img className="w-8 relative h-8" alt="" src="/group-8.svg" />
+          </div>
+        </div>
+        <div className="w-[711px] flex flex-row items-start justify-start gap-[121px] text-lg text-gray-300 font-body">
+          <div className="flex flex-col items-start justify-start gap-[32px]">
+            <div className="relative text-4xl leading-[120%] font-circular-std text-grey-scale-black">
+              Main Page
+            </div>
+            <div className="relative leading-[160%]">Destination</div>
+            <div className="relative leading-[160%]">Experiences</div>
+            <div className="relative leading-[160%]">Travel</div>
+          </div>
+          <div className="flex flex-col items-start justify-start gap-[32px]">
+            <div className="relative text-4xl leading-[120%] font-circular-std text-grey-scale-black">
+              Info
+            </div>
+            <div className="relative leading-[160%]">Why Nilgiris?</div>
+            <div className="relative leading-[160%]">Partner with us</div>
+            <div className="relative leading-[160%]">FAQ’s</div>
+            <div className="relative leading-[160%]">Blog</div>
+          </div>
+          <div className="flex flex-col items-start justify-start gap-[32px]">
+            <div className="relative text-4xl leading-[120%] font-circular-std text-grey-scale-black">
+              Helpline
+            </div>
+            <div className="relative leading-[160%]">+91 9488089404</div>
+            <div className="relative leading-[160%]">info@udaipur.com</div>
+          </div>
+        </div>
+      </div>
+      <img
+        className="absolute top-[-134px] left-[0px] w-[1512px] h-[979px] object-cover"
+        alt=""
+        src="/image-196@2x.png"
+      />
+    </div>
+  );
+};
+
+export default Form;
